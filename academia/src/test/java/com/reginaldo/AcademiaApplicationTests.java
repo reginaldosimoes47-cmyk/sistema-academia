358893da79115a100c86009a4be87d12b0138f48
reginaldosimoes47-cmyk <seu-email-do-github>
2026-05-01 14:28:25 -0300

    Step 5: Fix tests - Use H2 in-tests and add Postgres driver; Tests: 1/1 passed

--- academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
+++ academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
@@ -2,9 +2,10 @@ package com.reginaldo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import com.reginaldo.academia.AcademiaApplication;
 
-@SpringBootTest(classes = AcademiaApplication.class)
+@SpringBootTest(classes = AcademiaApplication.class, properties = {"spring.datasource.url=jdbc:h2:mem:testdb", "spring.datasource.driver-class-name=org.h2.Driver"})
 class AcademiaApplicationTests {
 
 	@Test
