0ed0ae3bb27f966401245d11fc673f0b850103b2
reginaldosimoes47-cmyk <seu-email-do-github>
2026-05-01 14:45:14 -0300

    Step 7: Test profile - add application-test.properties and activate @ActiveProfiles(test); Tests: passed

--- academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
+++ academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
@@ -2,10 +2,12 @@ package com.reginaldo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.reginaldo.academia.AcademiaApplication;
 
 @SpringBootTest(classes = AcademiaApplication.class)
+@ActiveProfiles("test")
 class AcademiaApplicationTests {
 
 	@Test
