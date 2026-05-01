af13a7cb8542cda89fa9ada49bbaee4edbc174a5
reginaldosimoes47-cmyk <seu-email-do-github>
2026-05-01 14:41:02 -0300

    Step 6: Add application-test.properties and revert inline test override; Tests passed

--- academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
+++ academia/src/test/java/com/reginaldo/AcademiaApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.reginaldo.academia.AcademiaApplication;
 
-@SpringBootTest(classes = AcademiaApplication.class, properties = {"spring.datasource.url=jdbc:h2:mem:testdb", "spring.datasource.driver-class-name=org.h2.Driver"})
+@SpringBootTest(classes = AcademiaApplication.class)
 class AcademiaApplicationTests {
 
 	@Test
